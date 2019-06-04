7ed7e5b17db83245d9a43949c69f4aec5cb8bfad
neerajsohal <hello@neerajkumar.name>
2019-06-04 12:02:08 +0530

    Bump version: 1.0.3 → 1.0.4

--- getsparrow.php
+++ getsparrow.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Sparrow
  * Plugin URI:        https://getsparrow.io/integrations/woocommerce
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
- * Version:           1.0.3
+ * Version:           1.0.4
  * Author:            Sparrow
  * Author URI:        https://getsparrow.io
  * License:           GPL-2.0+
