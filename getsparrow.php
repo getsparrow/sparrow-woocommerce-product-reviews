ea8a7a9795957c4022daadcc1a89299dc8355f65
neerajsohal <hello@neerajkumar.name>
2019-11-30 13:53:23 +0530

    added screenshots

--- getsparrow.php
+++ getsparrow.php
@@ -38,7 +38,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'COM_GETSPARROW_VERSION', '1.2.1' );
+define( 'COM_GETSPARROW_VERSION', '1.2.2' );
 
 /**
  * The code that runs during plugin activation.
