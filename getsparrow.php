617b63810b1560142babcad55c35b6085e06b53a
neerajsohal <hello@neerajkumar.name>
2019-11-30 14:38:07 +0530

    Bump version: 1.2.3 → 1.2.4

--- getsparrow.php
+++ getsparrow.php
@@ -16,7 +16,7 @@
  * Plugin Name:       Sparrow
  * Plugin URI:        https://getsparrow.io/integrations/woocommerce
  * Description:       Boost your store sales by UGC and social proof powered product reviews.
- * Version:           1.2.3
+ * Version:           1.2.4
  * Author:            Sparrow
  * Author URI:        https://getsparrow.io
  * License:           GPL-2.0+
