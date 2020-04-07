57f4cc7b0f39f271bbb176be911a5e2db675bd00
neerajsohal <hello@neerajkumar.name>
2020-04-08 02:37:19 +0530

    added additional check for valid product

--- public/class-getsparrow-public.php
+++ public/class-getsparrow-public.php
@@ -272,23 +272,25 @@ class Getsparrow_Public {
 	public function handle_sparrow_widgets_shortcode($attributes, $content = null) {
 		
 		global $product;
-		ob_start(); 
+		if(!is_null($product)) {
+			ob_start(); 
 		?>
 
-		<sparrow-reviews
-			data-access-token="<?php echo get_option('getsparrow_io_access_token'); ?>"
-			data-product-identifier="<?php echo $product->get_id(); ?>"
-			data-platform="woocommerce"
-			data-price="<?php echo (strlen($product->get_regular_price())? $product->get_regular_price() : 0); ?>"
-			data-sale-price="<?php echo (strlen($product->get_sale_price())? $product->get_sale_price() : $product->get_price()); ?>"
-			data-currency="<?php echo get_woocommerce_currency(); ?>"
-			data-name="<?php echo $product->get_name(); ?>"
-			data-url="<?php echo get_permalink($product->get_id()); ?>"
-			data-image-url="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'full' )[0]; ?>"
-			data-description="<?php echo (strlen($product->get_short_description())) ? $product->get_short_description() : ((strlen($product->get_description())) ? $product->get_description() : ''); ?>"
-		></sparrow-reviews>
+			<sparrow-reviews
+				data-access-token="<?php echo get_option('getsparrow_io_access_token'); ?>"
+				data-product-identifier="<?php echo $product->get_id(); ?>"
+				data-platform="woocommerce"
+				data-price="<?php echo (strlen($product->get_regular_price())? $product->get_regular_price() : 0); ?>"
+				data-sale-price="<?php echo (strlen($product->get_sale_price())? $product->get_sale_price() : $product->get_price()); ?>"
+				data-currency="<?php echo get_woocommerce_currency(); ?>"
+				data-name="<?php echo $product->get_name(); ?>"
+				data-url="<?php echo get_permalink($product->get_id()); ?>"
+				data-image-url="<?php echo wp_get_attachment_image_src( get_post_thumbnail_id( $product->get_id() ), 'full' )[0]; ?>"
+				data-description="<?php echo (strlen($product->get_short_description())) ? $product->get_short_description() : ((strlen($product->get_description())) ? $product->get_description() : ''); ?>"
+			></sparrow-reviews>
 
 		<?php 
+		}
 		return ob_get_clean();
 	}
 
